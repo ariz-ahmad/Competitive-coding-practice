009ee505fd88a085a162d01c2bdee4e1ff9271fc
ariz-ahmad <ariz.ahmad@ufl.edu>
2021-09-25 18:45:50 -0400

    Unit 1
    Sliding window

---
+++ 1.py
@@ -0,0 +1,393 @@
+def max_sub_array_of_size_k(k, arr):
+  max_sum = 0
+  window_sum = 0
+
+  for i in range(len(arr) - k + 1):
+    window_sum = 0
+    for j in range(i, i+k):
+      window_sum += arr[j]
+    max_sum = max(max_sum, window_sum)
+  return max_sum
+
+
+def main():
+  print("Maximum sum of a subarray of size K: " + str(max_sub_array_of_size_k(3, [2, 1, 5, 1, 3, 2])))
+  print("Maximum sum of a subarray of size K: " + str(max_sub_array_of_size_k(2, [2, 3, 4, 1, 5])))
+
+
+main()
+
+
+import math
+
+
+def smallest_subarray_with_given_sum(s, arr):
+  window_sum = 0
+  min_length = math.inf
+  window_start = 0
+
+  for window_end in range(0, len(arr)):
+    window_sum += arr[window_end]  # add the next element
+    # shrink the window as small as possible until the 'window_sum' is smaller than 's'
+    while window_sum >= s:
+      min_length = min(min_length, window_end - window_start + 1)
+      window_sum -= arr[window_start]
+      window_start += 1
+  if min_length == math.inf:
+    return 0
+  return min_length
+
+
+def main():
+  print("Smallest subarray length: " + str(smallest_subarray_with_given_sum(7, [2, 1, 5, 2, 3, 2])))
+  print("Smallest subarray length: " + str(smallest_subarray_with_given_sum(7, [2, 1, 5, 2, 8])))
+  print("Smallest subarray length: " + str(smallest_subarray_with_given_sum(8, [3, 4, 1, 1, 6])))
+
+
+main()
+
+
+def longest_substring_with_k_distinct(str1, k):
+  window_start = 0
+  max_length = 0
+  char_frequency = {}
+
+  # in the following loop we'll try to extend the range [window_start, window_end]
+  for window_end in range(len(str1)):
+    right_char = str1[window_end]
+    if right_char not in char_frequency:
+      char_frequency[right_char] = 0
+    char_frequency[right_char] += 1
+
+    # shrink the sliding window, until we are left with 'k' distinct characters in the char_frequency
+    while len(char_frequency) > k:
+      left_char = str1[window_start]
+      char_frequency[left_char] -= 1
+      if char_frequency[left_char] == 0:
+        del char_frequency[left_char]
+      window_start += 1  # shrink the window
+    # remember the maximum length so far
+    max_length = max(max_length, window_end-window_start + 1)
+  return max_length
+
+
+def main():
+  print("Length of the longest substring: " + str(longest_substring_with_k_distinct("araaci", 2)))
+  print("Length of the longest substring: " + str(longest_substring_with_k_distinct("araaci", 1)))
+  print("Length of the longest substring: " + str(longest_substring_with_k_distinct("cbbebi", 3)))
+
+
+main()
+
+
+def fruits_into_baskets(fruits):
+  window_start = 0
+  max_length = 0
+  fruit_frequency = {}
+
+  # try to extend the range [window_start, window_end]
+  for window_end in range(len(fruits)):
+    right_fruit = fruits[window_end]
+    if right_fruit not in fruit_frequency:
+      fruit_frequency[right_fruit] = 0
+    fruit_frequency[right_fruit] += 1
+
+    # shrink the sliding window, until we are left with '2' fruits in the fruit frequency dictionary
+    while len(fruit_frequency) > 2:
+      left_fruit = fruits[window_start]
+      fruit_frequency[left_fruit] -= 1
+      if fruit_frequency[left_fruit] == 0:
+        del fruit_frequency[left_fruit]
+      window_start += 1  # shrink the window
+    max_length = max(max_length, window_end-window_start + 1)
+  return max_length
+
+
+def main():
+  print("Maximum number of fruits: " + str(fruits_into_baskets(['A', 'B', 'C', 'A', 'C'])))
+  print("Maximum number of fruits: " + str(fruits_into_baskets(['A', 'B', 'C', 'B', 'B', 'C'])))
+
+
+main()
+
+
+def non_repeat_substring(str1):
+  window_start = 0
+  max_length = 0
+  char_index_map = {}
+
+  # try to extend the range [windowStart, windowEnd]
+  for window_end in range(len(str1)):
+    right_char = str1[window_end]
+    # if the map already contains the 'right_char', shrink the window from the beginning so that
+    # we have only one occurrence of 'right_char'
+    if right_char in char_index_map:
+      # this is tricky; in the current window, we will not have any 'right_char' after its previous index
+      # and if 'window_start' is already ahead of the last index of 'right_char', we'll keep 'window_start'
+      window_start = max(window_start, char_index_map[right_char] + 1)
+    # insert the 'right_char' into the map
+    char_index_map[right_char] = window_end
+    # remember the maximum length so far
+    max_length = max(max_length, window_end - window_start + 1)
+  return max_length
+
+
+def main():
+  print("Length of the longest substring: " + str(non_repeat_substring("aabccbb")))
+  print("Length of the longest substring: " + str(non_repeat_substring("abbbb")))
+  print("Length of the longest substring: " + str(non_repeat_substring("abccde")))
+
+
+main()
+
+
+def length_of_longest_substring(str1, k):
+  window_start, max_length, max_repeat_letter_count = 0, 0, 0
+  frequency_map = {}
+
+  # Try to extend the range [window_start, window_end]
+  for window_end in range(len(str1)):
+    right_char = str1[window_end]
+    if right_char not in frequency_map:
+      frequency_map[right_char] = 0
+    frequency_map[right_char] += 1
+    max_repeat_letter_count = max(
+      max_repeat_letter_count, frequency_map[right_char])
+
+    # Current window size is from window_start to window_end, overall we have a letter which is
+    # repeating 'max_repeat_letter_count' times, this means we can have a window which has one letter
+    # repeating 'max_repeat_letter_count' times and the remaining letters we should replace.
+    # if the remaining letters are more than 'k', it is the time to shrink the window as we
+    # are not allowed to replace more than 'k' letters
+    if (window_end - window_start + 1 - max_repeat_letter_count) > k:
+      left_char = str1[window_start]
+      frequency_map[left_char] -= 1
+      window_start += 1
+
+    max_length = max(max_length, window_end - window_start + 1)
+  return max_length
+
+
+def main():
+  print(length_of_longest_substring("aabccbb", 2))
+  print(length_of_longest_substring("abbcb", 1))
+  print(length_of_longest_substring("abccde", 1))
+
+
+main()
+
+
+
+def length_of_longest_substring(arr, k):
+  window_start, max_length, max_ones_count = 0, 0, 0
+
+  # Try to extend the range [window_start, window_end]
+  for window_end in range(len(arr)):
+    if arr[window_end] == 1:
+      max_ones_count += 1
+
+    # Current window size is from window_start to window_end, overall we have a maximum of 1s
+    # repeating 'max_ones_count' times, this means we can have a window with 'max_ones_count' 1s
+    # and the remaining are 0s which should replace with 1s.
+    # now, if the remaining 0s are more than 'k', it is the time to shrink the window as we
+    # are not allowed to replace more than 'k' 0s
+    if (window_end - window_start + 1 - max_ones_count) > k:
+      if arr[window_start] == 1:
+        max_ones_count -= 1
+      window_start += 1
+
+    max_length = max(max_length, window_end - window_start + 1)
+  return max_length
+
+
+def main():
+  print(length_of_longest_substring([0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1], 2))
+  print(length_of_longest_substring(
+    [0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1], 3))
+
+
+main()
+
+
+
+def find_permutation(str1, pattern):
+  window_start, matched = 0, 0
+  char_frequency = {}
+
+  for chr in pattern:
+    if chr not in char_frequency:
+      char_frequency[chr] = 0
+    char_frequency[chr] += 1
+
+  # our goal is to match all the characters from the 'char_frequency' with the current window
+  # try to extend the range [window_start, window_end]
+  for window_end in range(len(str1)):
+    right_char = str1[window_end]
+    if right_char in char_frequency:
+      # decrement the frequency of matched character
+      char_frequency[right_char] -= 1
+      if char_frequency[right_char] == 0:
+        matched += 1
+
+    if matched == len(char_frequency):
+      return True
+
+    # shrink the window by one character
+    if window_end >= len(pattern) - 1:
+      left_char = str1[window_start]
+      window_start += 1
+      if left_char in char_frequency:
+        if char_frequency[left_char] == 0:
+          matched -= 1
+        char_frequency[left_char] += 1
+
+  return False
+
+
+def main():
+  print('Permutation exist: ' + str(find_permutation("oidbcaf", "abc")))
+  print('Permutation exist: ' + str(find_permutation("odicf", "dc")))
+  print('Permutation exist: ' + str(find_permutation("bcdxabcdy", "bcdyabcdx")))
+  print('Permutation exist: ' + str(find_permutation("aaacb", "abc")))
+
+
+main()
+
+
+
+def find_string_anagrams(str1, pattern):
+  window_start, matched = 0, 0
+  char_frequency = {}
+
+  for chr in pattern:
+    if chr not in char_frequency:
+      char_frequency[chr] = 0
+    char_frequency[chr] += 1
+
+  result_indices = []
+  # Our goal is to match all the characters from the 'char_frequency' with the current window
+  # try to extend the range [window_start, window_end]
+  for window_end in range(len(str1)):
+    right_char = str1[window_end]
+    if right_char in char_frequency:
+      # Decrement the frequency of matched character
+      char_frequency[right_char] -= 1
+      if char_frequency[right_char] == 0:
+        matched += 1
+
+    if matched == len(char_frequency):  # Have we found an anagram?
+      result_indices.append(window_start)
+
+    # Shrink the sliding window
+    if window_end >= len(pattern) - 1:
+      left_char = str1[window_start]
+      window_start += 1
+      if left_char in char_frequency:
+        if char_frequency[left_char] == 0:
+          matched -= 1  # Before putting the character back, decrement the matched count
+        char_frequency[left_char] += 1  # Put the character back
+
+  return result_indices
+
+
+def main():
+  print(find_string_anagrams("ppqp", "pq"))
+  print(find_string_anagrams("abbcabc", "abc"))
+
+
+main()
+
+
+
+def find_substring(str1, pattern):
+  window_start, matched, substr_start = 0, 0, 0
+  min_length = len(str1) + 1
+  char_frequency = {}
+
+  for chr in pattern:
+    if chr not in char_frequency:
+      char_frequency[chr] = 0
+    char_frequency[chr] += 1
+
+  # try to extend the range [window_start, window_end]
+  for window_end in range(len(str1)):
+    right_char = str1[window_end]
+    if right_char in char_frequency:
+      char_frequency[right_char] -= 1
+      if char_frequency[right_char] >= 0:  # Count every matching of a character
+        matched += 1
+
+    # Shrink the window if we can, finish as soon as we remove a matched character
+    while matched == len(pattern):
+      if min_length > window_end - window_start + 1:
+        min_length = window_end - window_start + 1
+        substr_start = window_start
+
+      left_char = str1[window_start]
+      window_start += 1
+      if left_char in char_frequency:
+        # Note that we could have redundant matching characters, therefore we'll decrement the
+        # matched count only when a useful occurrence of a matched character is going out of the window
+        if char_frequency[left_char] == 0:
+          matched -= 1
+        char_frequency[left_char] += 1
+
+  if min_length > len(str1):
+    return ""
+  return str1[substr_start:substr_start + min_length]
+
+
+def main():
+  print(find_substring("aabdec", "abc"))
+  print(find_substring("abdbca", "abc"))
+  print(find_substring("adcad", "abc"))
+
+main()
+
+
+
+def find_word_concatenation(str1, words):
+  if len(words) == 0 or len(words[0]) == 0:
+    return []
+
+  word_frequency = {}
+
+  for word in words:
+    if word not in word_frequency:
+      word_frequency[word] = 0
+    word_frequency[word] += 1
+
+  result_indices = []
+  words_count = len(words)
+  word_length = len(words[0])
+
+  for i in range((len(str1) - words_count * word_length)+1):
+    words_seen = {}
+    for j in range(0, words_count):
+      next_word_index = i + j * word_length
+      # Get the next word from the string
+      word = str1[next_word_index: next_word_index + word_length]
+      if word not in word_frequency:  # Break if we don't need this word
+        break
+
+      # Add the word to the 'words_seen' map
+      if word not in words_seen:
+        words_seen[word] = 0
+      words_seen[word] += 1
+
+      # No need to process further if the word has higher frequency than required
+      if words_seen[word] > word_frequency.get(word, 0):
+        break
+
+      if j + 1 == words_count:  # Store index if we have found all the words
+        result_indices.append(i)
+
+  return result_indices
+
+
+def main():
+  print(find_word_concatenation("catfoxcat", ["cat", "fox"]))
+  print(find_word_concatenation("catcatfoxfox", ["cat", "fox"]))
+
+
+main()
