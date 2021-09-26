650598c7d14ad78062d3c272d08ce476f1410008
ariz-ahmad <ariz.ahmad@ufl.edu>
2021-09-25 20:29:09 -0400

    Unit 7 Tree Breadth First Search

---
+++ 7.py
@@ -0,0 +1,470 @@
+from collections import deque
+
+
+class TreeNode:
+  def __init__(self, val):
+    self.val = val
+    self.left, self.right = None, None
+
+
+def traverse(root):
+  result = []
+  if root is None:
+    return result
+
+  queue = deque()
+  queue.append(root)
+  while queue:
+    levelSize = len(queue)
+    currentLevel = []
+    for _ in range(levelSize):
+      currentNode = queue.popleft()
+      # add the node to the current level
+      currentLevel.append(currentNode.val)
+      # insert the children of current node in the queue
+      if currentNode.left:
+        queue.append(currentNode.left)
+      if currentNode.right:
+        queue.append(currentNode.right)
+
+    result.append(currentLevel)
+
+  return result
+
+
+def main():
+  root = TreeNode(12)
+  root.left = TreeNode(7)
+  root.right = TreeNode(1)
+  root.left.left = TreeNode(9)
+  root.right.left = TreeNode(10)
+  root.right.right = TreeNode(5)
+  print("Level order traversal: " + str(traverse(root)))
+
+
+main()
+
+
+
+from collections import deque
+
+
+class TreeNode:
+  def __init__(self, val):
+    self.val = val
+    self.left, self.right = None, None
+
+
+def traverse(root):
+  result = deque()
+  if root is None:
+    return result
+
+  queue = deque()
+  queue.append(root)
+  while queue:
+    levelSize = len(queue)
+    currentLevel = []
+    for _ in range(levelSize):
+      currentNode = queue.popleft()
+      # add the node to the current level
+      currentLevel.append(currentNode.val)
+      # insert the children of current node in the queue
+      if currentNode.left:
+        queue.append(currentNode.left)
+      if currentNode.right:
+        queue.append(currentNode.right)
+
+    result.appendleft(currentLevel)
+
+  return result
+
+
+def main():
+  root = TreeNode(12)
+  root.left = TreeNode(7)
+  root.right = TreeNode(1)
+  root.left.left = TreeNode(9)
+  root.right.left = TreeNode(10)
+  root.right.right = TreeNode(5)
+  print("Reverse level order traversal: " + str(traverse(root)))
+
+
+main()
+
+
+
+from collections import deque
+
+
+class TreeNode:
+  def __init__(self, val):
+    self.val = val
+    self.left, self.right = None, None
+
+
+def traverse(root):
+  result = []
+  if root is None:
+    return result
+
+  queue = deque()
+  queue.append(root)
+  leftToRight = True
+  while queue:
+    levelSize = len(queue)
+    currentLevel = deque()
+    for _ in range(levelSize):
+      currentNode = queue.popleft()
+
+      # add the node to the current level based on the traverse direction
+      if leftToRight:
+        currentLevel.append(currentNode.val)
+      else:
+        currentLevel.appendleft(currentNode.val)
+
+      # insert the children of current node in the queue
+      if currentNode.left:
+        queue.append(currentNode.left)
+      if currentNode.right:
+        queue.append(currentNode.right)
+
+    result.append(list(currentLevel))
+    # reverse the traversal direction
+    leftToRight = not leftToRight
+
+  return result
+
+
+def main():
+  root = TreeNode(12)
+  root.left = TreeNode(7)
+  root.right = TreeNode(1)
+  root.left.left = TreeNode(9)
+  root.right.left = TreeNode(10)
+  root.right.right = TreeNode(5)
+  root.right.left.left = TreeNode(20)
+  root.right.left.right = TreeNode(17)
+  print("Zigzag traversal: " + str(traverse(root)))
+
+
+main()
+
+
+
+from collections import deque
+
+
+class TreeNode:
+  def __init__(self, val):
+    self.val = val
+    self.left, self.right = None, None
+
+
+def find_level_averages(root):
+  result = []
+  if root is None:
+    return result
+
+  queue = deque()
+  queue.append(root)
+  while queue:
+    levelSize = len(queue)
+    levelSum = 0.0
+    for _ in range(levelSize):
+      currentNode = queue.popleft()
+      # add the node's value to the running sum
+      levelSum += currentNode.val
+      # insert the children of current node to the queue
+      if currentNode.left:
+        queue.append(currentNode.left)
+      if currentNode.right:
+        queue.append(currentNode.right)
+
+    # append the current level's average to the result array
+    result.append(levelSum / levelSize)
+
+  return result
+
+
+def main():
+  root = TreeNode(12)
+  root.left = TreeNode(7)
+  root.right = TreeNode(1)
+  root.left.left = TreeNode(9)
+  root.left.right = TreeNode(2)
+  root.right.left = TreeNode(10)
+  root.right.right = TreeNode(5)
+  print("Level averages are: " + str(find_level_averages(root)))
+
+
+main()
+
+
+
+from collections import deque
+
+
+class TreeNode:
+  def __init__(self, val):
+    self.val = val
+    self.left, self.right = None, None
+
+
+def find_maximum_depth(root):
+  if root is None:
+    return 0
+
+  queue = deque()
+  queue.append(root)
+  maximumTreeDepth = 0
+  while queue:
+    maximumTreeDepth += 1
+    levelSize = len(queue)
+    for _ in range(levelSize):
+      currentNode = queue.popleft()
+
+      # insert the children of current node in the queue
+      if currentNode.left:
+        queue.append(currentNode.left)
+      if currentNode.right:
+        queue.append(currentNode.right)
+
+  return maximumTreeDepth
+
+
+def main():
+  root = TreeNode(12)
+  root.left = TreeNode(7)
+  root.right = TreeNode(1)
+  root.right.left = TreeNode(10)
+  root.right.right = TreeNode(5)
+  print("Tree Maximum Depth: " + str(find_maximum_depth(root)))
+  root.left.left = TreeNode(9)
+  root.right.left.left = TreeNode(11)
+  print("Tree Maximum Depth: " + str(find_maximum_depth(root)))
+
+
+main()
+
+
+
+from collections import deque
+
+
+class TreeNode:
+  def __init__(self, val):
+    self.val = val
+    self.left, self.right = None, None
+
+
+def find_successor(root, key):
+  if root is None:
+    return None
+
+  queue = deque()
+  queue.append(root)
+  while queue:
+    currentNode = queue.popleft()
+    # insert the children of current node in the queue
+    if currentNode.left:
+      queue.append(currentNode.left)
+    if currentNode.right:
+      queue.append(currentNode.right)
+
+    # break if we have found the key
+    if currentNode.val == key:
+      break
+
+  return queue[0] if queue else None
+
+
+def main():
+  root = TreeNode(12)
+  root.left = TreeNode(7)
+  root.right = TreeNode(1)
+  root.left.left = TreeNode(9)
+  root.right.left = TreeNode(10)
+  root.right.right = TreeNode(5)
+  result = find_successor(root, 12)
+  if result:
+    print(result.val)
+  result = find_successor(root, 9)
+  if result:
+    print(result.val)
+
+
+main()
+
+
+
+from __future__ import print_function
+from collections import deque
+
+
+class TreeNode:
+  def __init__(self, val):
+    self.val = val
+    self.left, self.right, self.next = None, None, None
+
+  # level order traversal using 'next' pointer
+  def print_level_order(self):
+    nextLevelRoot = self
+    while nextLevelRoot:
+      current = nextLevelRoot
+      nextLevelRoot = None
+      while current:
+        print(str(current.val) + " ", end='')
+        if not nextLevelRoot:
+          if current.left:
+            nextLevelRoot = current.left
+          elif current.right:
+            nextLevelRoot = current.right
+        current = current.next
+      print()
+
+
+def connect_level_order_siblings(root):
+  if root is None:
+    return
+
+  queue = deque()
+  queue.append(root)
+  while queue:
+    previousNode = None
+    levelSize = len(queue)
+    # connect all nodes of this level
+    for _ in range(levelSize):
+      currentNode = queue.popleft()
+      if previousNode:
+        previousNode.next = currentNode
+      previousNode = currentNode
+
+      # insert the children of current node in the queue
+      if currentNode.left:
+        queue.append(currentNode.left)
+      if currentNode.right:
+        queue.append(currentNode.right)
+
+
+def main():
+  root = TreeNode(12)
+  root.left = TreeNode(7)
+  root.right = TreeNode(1)
+  root.left.left = TreeNode(9)
+  root.right.left = TreeNode(10)
+  root.right.right = TreeNode(5)
+  connect_level_order_siblings(root)
+
+  print("Level order traversal using 'next' pointer: ")
+  root.print_level_order()
+
+
+main()
+
+
+
+from __future__ import print_function
+from collections import deque
+
+
+class TreeNode:
+  def __init__(self, val):
+    self.val = val
+    self.left, self.right, self.next = None, None, None
+
+  # tree traversal using 'next' pointer
+  def print_tree(self):
+    print("Traversal using 'next' pointer: ", end='')
+    current = self
+    while current:
+      print(str(current.val) + " ", end='')
+      current = current.next
+
+
+def connect_all_siblings(root):
+  if root is None:
+    return
+
+  queue = deque()
+  queue.append(root)
+  currentNode, previousNode = None, None
+  while queue:
+    currentNode = queue.popleft()
+    if previousNode:
+      previousNode.next = currentNode
+    previousNode = currentNode
+
+    # insert the children of current node in the queue
+    if currentNode.left:
+      queue.append(currentNode.left)
+    if currentNode.right:
+      queue.append(currentNode.right)
+
+
+def main():
+  root = TreeNode(12)
+  root.left = TreeNode(7)
+  root.right = TreeNode(1)
+  root.left.left = TreeNode(9)
+  root.right.left = TreeNode(10)
+  root.right.right = TreeNode(5)
+  connect_all_siblings(root)
+  root.print_tree()
+
+
+main()
+
+
+
+from __future__ import print_function
+from collections import deque
+
+
+class TreeNode:
+  def __init__(self, val):
+    self.val = val
+    self.left, self.right = None, None
+
+
+def tree_right_view(root):
+  result = []
+  if root is None:
+    return result
+
+  queue = deque()
+  queue.append(root)
+  while queue:
+    levelSize = len(queue)
+    for i in range(0, levelSize):
+      currentNode = queue.popleft()
+      # if it is the last node of this level, add it to the result
+      if i == levelSize - 1:
+        result.append(currentNode)
+      # insert the children of current node in the queue
+      if currentNode.left:
+        queue.append(currentNode.left)
+      if currentNode.right:
+        queue.append(currentNode.right)
+
+  return result
+
+
+def main():
+  root = TreeNode(12)
+  root.left = TreeNode(7)
+  root.right = TreeNode(1)
+  root.left.left = TreeNode(9)
+  root.right.left = TreeNode(10)
+  root.right.right = TreeNode(5)
+  root.left.left.left = TreeNode(3)
+  result = tree_right_view(root)
+  print("Tree right view: ")
+  for node in result:
+    print(str(node.val) + " ", end='')
+
+
+main()
+
+
+
