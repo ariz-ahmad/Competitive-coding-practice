626cd2a7315031bcb3ca39d5b038d4f73baad1f6
ariz-ahmad <ariz.ahmad@ufl.edu>
2021-09-25 20:31:51 -0400

    Unit 8 Tree Depth First Search

---
+++ 8.py
@@ -0,0 +1,346 @@
+class TreeNode:
+  def __init__(self, val, left=None, right=None):
+    self.val = val
+    self.left = left
+    self.right = right
+
+
+def has_path(root, sum):
+  if root is None:
+    return False
+
+  # if the current node is a leaf and its value is equal to the sum, we've found a path
+  if root.val == sum and root.left is None and root.right is None:
+    return True
+
+  # recursively call to traverse the left and right sub-tree
+  # return true if any of the two recursive call return true
+  return has_path(root.left, sum - root.val) or has_path(root.right, sum - root.val)
+
+
+def main():
+
+  root = TreeNode(12)
+  root.left = TreeNode(7)
+  root.right = TreeNode(1)
+  root.left.left = TreeNode(9)
+  root.right.left = TreeNode(10)
+  root.right.right = TreeNode(5)
+  print("Tree has path: " + str(has_path(root, 23)))
+  print("Tree has path: " + str(has_path(root, 16)))
+
+
+main()
+
+
+
+class TreeNode:
+  def __init__(self, val, left=None, right=None):
+    self.val = val
+    self.left = left
+    self.right = right
+
+
+def find_paths(root, required_sum):
+  allPaths = []
+  find_paths_recursive(root, required_sum, [], allPaths)
+  return allPaths
+
+
+def find_paths_recursive(currentNode, required_sum, currentPath, allPaths):
+  if currentNode is None:
+    return
+
+  # add the current node to the path
+  currentPath.append(currentNode.val)
+
+  # if the current node is a leaf and its value is equal to required_sum, save the current path
+  if currentNode.val == required_sum and currentNode.left is None and currentNode.right is None:
+    allPaths.append(list(currentPath))
+  else:
+    # traverse the left sub-tree
+    find_paths_recursive(currentNode.left, required_sum -
+                         currentNode.val, currentPath, allPaths)
+    # traverse the right sub-tree
+    find_paths_recursive(currentNode.right, required_sum -
+                         currentNode.val, currentPath, allPaths)
+
+  # remove the current node from the path to backtrack,
+  # we need to remove the current node while we are going up the recursive call stack.
+  del currentPath[-1]
+
+
+def main():
+
+  root = TreeNode(12)
+  root.left = TreeNode(7)
+  root.right = TreeNode(1)
+  root.left.left = TreeNode(4)
+  root.right.left = TreeNode(10)
+  root.right.right = TreeNode(5)
+  required_sum = 23
+  print("Tree paths with required_sum " + str(required_sum) +
+        ": " + str(find_paths(root, required_sum)))
+
+
+main()
+
+
+
+class TreeNode:
+  def __init__(self, val, left=None, right=None):
+    self.val = val
+    self.left = left
+    self.right = right
+
+
+def find_sum_of_path_numbers(root):
+  return find_root_to_leaf_path_numbers(root, 0)
+
+
+def find_root_to_leaf_path_numbers(currentNode, pathSum):
+  if currentNode is None:
+    return 0
+
+  # calculate the path number of the current node
+  pathSum = 10 * pathSum + currentNode.val
+
+  # if the current node is a leaf, return the current path sum
+  if currentNode.left is None and currentNode.right is None:
+    return pathSum
+
+  # traverse the left and the right sub-tree
+  return find_root_to_leaf_path_numbers(currentNode.left, pathSum) + find_root_to_leaf_path_numbers(currentNode.right, pathSum)
+
+
+def main():
+  root = TreeNode(1)
+  root.left = TreeNode(0)
+  root.right = TreeNode(1)
+  root.left.left = TreeNode(1)
+  root.right.left = TreeNode(6)
+  root.right.right = TreeNode(5)
+  print("Total Sum of Path Numbers: " + str(find_sum_of_path_numbers(root)))
+
+
+main()
+
+
+
+class TreeNode:
+  def __init__(self, val, left=None, right=None):
+    self.val = val
+    self.left = left
+    self.right = right
+
+
+def find_path(root, sequence):
+  if not root:
+    return len(sequence) == 0
+
+  return find_path_recursive(root, sequence, 0)
+
+
+def find_path_recursive(currentNode, sequence, sequenceIndex):
+
+  if currentNode is None:
+    return False
+
+  seqLen = len(sequence)
+  if sequenceIndex >= seqLen or currentNode.val != sequence[sequenceIndex]:
+    return False
+
+  # if the current node is a leaf, add it is the end of the sequence, we have found a path!
+  if currentNode.left is None and currentNode.right is None and sequenceIndex == seqLen - 1:
+    return True
+
+  # recursively call to traverse the left and right sub-tree
+  # return true if any of the two recursive call return true
+  return find_path_recursive(currentNode.left, sequence, sequenceIndex + 1) or \
+         find_path_recursive(currentNode.right, sequence, sequenceIndex + 1)
+
+
+def main():
+
+  root = TreeNode(1)
+  root.left = TreeNode(0)
+  root.right = TreeNode(1)
+  root.left.left = TreeNode(1)
+  root.right.left = TreeNode(6)
+  root.right.right = TreeNode(5)
+
+  print("Tree has path sequence: " + str(find_path(root, [1, 0, 7])))
+  print("Tree has path sequence: " + str(find_path(root, [1, 1, 6])))
+
+
+main()
+
+
+
+class TreeNode:
+  def __init__(self, val, left=None, right=None):
+    self.val = val
+    self.left = left
+    self.right = right
+
+
+def count_paths(root, S):
+  return count_paths_recursive(root, S, [])
+
+
+def count_paths_recursive(currentNode, S, currentPath):
+  if currentNode is None:
+    return 0
+
+  # add the current node to the path
+  currentPath.append(currentNode.val)
+  pathCount, pathSum = 0, 0
+  # find the sums of all sub-paths in the current path list
+  for i in range(len(currentPath)-1, -1, -1):
+    pathSum += currentPath[i]
+    # if the sum of any sub-path is equal to 'S' we increment our path count.
+    if pathSum == S:
+      pathCount += 1
+
+  # traverse the left sub-tree
+  pathCount += count_paths_recursive(currentNode.left, S, currentPath)
+  # traverse the right sub-tree
+  pathCount += count_paths_recursive(currentNode.right, S, currentPath)
+
+  # remove the current node from the path to backtrack
+  # we need to remove the current node while we are going up the recursive call stack
+  del currentPath[-1]
+
+  return pathCount
+
+
+def main():
+  root = TreeNode(12)
+  root.left = TreeNode(7)
+  root.right = TreeNode(1)
+  root.left.left = TreeNode(4)
+  root.right.left = TreeNode(10)
+  root.right.right = TreeNode(5)
+  print("Tree has paths: " + str(count_paths(root, 11)))
+
+
+main()
+
+
+
+class TreeNode:
+  def __init__(self, val, left=None, right=None):
+    self.val = val
+    self.left = left
+    self.right = right
+
+
+def count_paths(root, S):
+  return count_paths_recursive(root, S, [])
+
+
+def count_paths_recursive(currentNode, S, currentPath):
+  if currentNode is None:
+    return 0
+
+  # add the current node to the path
+  currentPath.append(currentNode.val)
+  pathCount, pathSum = 0, 0
+  # find the sums of all sub-paths in the current path list
+  for i in range(len(currentPath)-1, -1, -1):
+    pathSum += currentPath[i]
+    # if the sum of any sub-path is equal to 'S' we increment our path count.
+    if pathSum == S:
+      pathCount += 1
+
+  # traverse the left sub-tree
+  pathCount += count_paths_recursive(currentNode.left, S, currentPath)
+  # traverse the right sub-tree
+  pathCount += count_paths_recursive(currentNode.right, S, currentPath)
+
+  # remove the current node from the path to backtrack
+  # we need to remove the current node while we are going up the recursive call stack
+  del currentPath[-1]
+
+  return pathCount
+
+
+def main():
+  root = TreeNode(12)
+  root.left = TreeNode(7)
+  root.right = TreeNode(1)
+  root.left.left = TreeNode(4)
+  root.right.left = TreeNode(10)
+  root.right.right = TreeNode(5)
+  print("Tree has paths: " + str(count_paths(root, 11)))
+
+
+main()
+
+
+
+import math
+
+
+class TreeNode:
+  def __init__(self, val, left=None, right=None):
+    self.val = val
+    self.left = left
+    self.right = right
+
+
+class MaximumPathSum:
+
+  def find_maximum_path_sum(self, root):
+    self.globalMaximumSum = -math.inf
+    self.find_maximum_path_sum_recursive(root)
+    return self.globalMaximumSum
+
+  def find_maximum_path_sum_recursive(self, currentNode):
+    if currentNode is None:
+      return 0
+
+    maxPathSumFromLeft = self.find_maximum_path_sum_recursive(
+      currentNode.left)
+    maxPathSumFromRight = self.find_maximum_path_sum_recursive(
+      currentNode.right)
+
+    # ignore paths with negative sums, since we need to find the maximum sum we should
+    # ignore any path which has an overall negative sum.
+    maxPathSumFromLeft = max(maxPathSumFromLeft, 0)
+    maxPathSumFromRight = max(maxPathSumFromRight, 0)
+
+    # maximum path sum at the current node will be equal to the sum from the left subtree +
+    # the sum from right subtree + val of current node
+    localMaximumSum = maxPathSumFromLeft + maxPathSumFromRight + currentNode.val
+
+    # update the global maximum sum
+    self.globalMaximumSum = max(self.globalMaximumSum, localMaximumSum)
+
+    # maximum sum of any path from the current node will be equal to the maximum of
+    # the sums from left or right subtrees plus the value of the current node
+    return max(maxPathSumFromLeft, maxPathSumFromRight) + currentNode.val
+
+
+def main():
+  maximumPathSum = MaximumPathSum()
+  root = TreeNode(1)
+  root.left = TreeNode(2)
+  root.right = TreeNode(3)
+
+  print("Maximum Path Sum: " + str(maximumPathSum.find_maximum_path_sum(root)))
+  root.left.left = TreeNode(1)
+  root.left.right = TreeNode(3)
+  root.right.left = TreeNode(5)
+  root.right.right = TreeNode(6)
+  root.right.left.left = TreeNode(7)
+  root.right.left.right = TreeNode(8)
+  root.right.right.left = TreeNode(9)
+  print("Maximum Path Sum: " + str(maximumPathSum.find_maximum_path_sum(root)))
+
+  root = TreeNode(-1)
+  root.left = TreeNode(-3)
+  print("Maximum Path Sum: " + str(maximumPathSum.find_maximum_path_sum(root)))
+
+
+main()
